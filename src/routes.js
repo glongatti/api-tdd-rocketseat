de51ae50fa07f6d0f8f6e509ec0ae60496f4b9e8
Gabriel Longatti <glongatti@masti.com.br>
2019-09-23 09:56:00 -0300

    - Removed all old files

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router();
-const SessionsController = require('./app/controllers/SessionController')
-
-routes.post('/sessions', SessionsController.store);
-
-
-module.exports = routes;
